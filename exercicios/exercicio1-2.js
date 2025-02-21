39b63986a8ec2902345155f01bea7ad53835ed00
PedroMunizSENAI <githubmuniz@gmail.com>
2025-02-21 14:12:31 -0300

    initial commit

---
+++ exercicios/exercicio1-2.js
@@ -0,0 +1,22 @@
+let prompt = require("prompt-sync");
+prompt = prompt();
+
+//EXERCICIO 1.1
+// let preco = parseFloat(prompt("Qual preço do produto? "));
+// let desconto = parseFloat(prompt("Qual valor do desconto? "));
+
+// function valor(preco, desconto) {
+//   const calcDesconto = preco - (preco * desconto) / 100;
+//   console.log("O valor com desconto é ", calcDesconto);
+// }
+// valor(preco, desconto);
+
+//EXERCICIO 1.2
+
+// let temperaturaCel = parseFloat(prompt("Qual a temperatura em C° "))
+
+// function converter(temperaturaCel) {
+//     const temperaturaFr = temperaturaCel * 1.8 + 32;
+//     console.log("Convertendo a temperatura celcius em FR é igual a ", temperaturaFr,"°")
+// }
+// converter(temperaturaCel)
